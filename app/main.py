9c25fab0dd65e861c6bf5d93e1ac3f1d78ae6619
Arun Sabu <sabuarun00@gmail.com>
2025-10-24 19:27:33 +0530

    Update main.py with frontend setup

--- app/main.py
+++ app/main.py
@@ -6,6 +6,10 @@ from sqlalchemy.orm import relationship
 from models import Student, Course, Enrollment 
 from schemas import StudentCreate, StudentUpdate, StudentsBulk, CourseCreate, EnrollmentCreate
 from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi import Request
 
 app = FastAPI()
 
@@ -15,6 +19,18 @@ def get_db():
         yield db
     finally:
         db.close()
+        
+        
+# Serve static files (CSS/JS)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Templates folder
+templates = Jinja2Templates(directory="templates")
+
+# Home page
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
         
 # ---------- Students CRUD ----------
         
